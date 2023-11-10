069480386a5fbed5661d91767bf12a8742c5d6fd
Aaron Sheldon <aaronsheldon@users.noreply.github.com>
2023-11-10 15:56:03 -0700

    Automatic commit from Bubble

--- actions/ACW-851ne/server.js
+++ actions/ACW-851ne/server.js
@@ -1,9 +1,9 @@
-function(properties, context) {
-    var parseutil = require("mailparser");
+async function(properties, context) {
+    const parseutil = require("mailparser");
     
     // Concise names
-    var emailbuffer = Buffer.from(properties.rawemail, "base64");
-    var parseoptions = {
+    const emailbuffer = Buffer.from(properties.rawemail, "base64");
+    const parseoptions = {
         skipHtmlToText: true,
         skipImageLinks: false,
         skipTextToHtml: false,
@@ -12,27 +12,27 @@ function(properties, context) {
     };
     
     // Parse the buffer
-    var promiseparse = parseutil.simpleParser(emailbuffer, parseoptions);
-    var parsedemail = context.async(
-        callback => promiseparse
-        .then(parsed => callback(null, parsed))
-        .catch(reason => callback(reason))
+    const promiseparse = parseutil.simpleParser(emailbuffer, parseoptions)
+    .then(
+        (parsed) => {
+        	return {
+                headersubject: !parsed.subject ? null : parsed.subject,
+                headerfrom: !parsed.from ? [] : parsed.from.value.map(a => a.address),
+                headerto: !parsed.to ? [] : parsed.to.value.map(a => a.address),
+                headercc: !parsed.cc ? [] : parsed.cc.value.map(a => a.address),
+                headerbcc: !parsed.bcc ? [] : parsed.bcc.value.map(a => a.address),
+                headerdate: !parsed.date ? null : parsed.date,
+                headermessageid: !parsed.messageId ? null : parsed.messageId,
+                headerinreplyto: !parsed.inReplyTo ? null : parsed.inReplyTo,
+                headerreplyto: !parsed.replyTo ? [] : parsed.replyTo.value.map(a => a.address),
+                bodyhtml: !parsed.html ? null : parsed.html,
+                attachmentnames: !parsed.attachments ? [] : parsed.attachments.map(a => a.filename || " "),
+                attachmenttypes: !parsed.attachments ? [] : parsed.attachments.map(a => a.contentType || " "),
+                attachmentccontents: !parsed.attachments ? [] : parsed.attachments.map(a => a.content.toString("Base64") || " ")    
+            };
+        }
     );
     
  	// Send
-    return {
-        headersubject: !parsedemail.subject ? null : parsedemail.subject,
-        headerfrom: !parsedemail.from ? [] : parsedemail.from.value.map(a => a.address),
-        headerto: !parsedemail.to ? [] : parsedemail.to.value.map(a => a.address),
-        headercc: !parsedemail.cc ? [] : parsedemail.cc.value.map(a => a.address),
-        headerbcc: !parsedemail.bcc ? [] : parsedemail.bcc.value.map(a => a.address),
-        headerdate: !parsedemail.date ? null : parsedemail.date,
-        headermessageid: !parsedemail.messageId ? null : parsedemail.messageId,
-        headerinreplyto: !parsedemail.inReplyTo ? null : parsedemail.inReplyTo,
-        headerreplyto: !parsedemail.replyTo ? [] : parsedemail.replyTo.value.map(a => a.address),
-        bodyhtml: !parsedemail.html ? null : parsedemail.html,
-        attachmentnames: !parsedemail.attachments ? [] : parsedemail.attachments.map(a => a.filename || " "),
-        attachmenttypes: !parsedemail.attachments ? [] : parsedemail.attachments.map(a => a.contentType || " "),
-        attachmentccontents: !parsedemail.attachments ? [] : parsedemail.attachments.map(a => a.content.toString("Base64") || " ")    
-    };
+    return promiseparse;
 }
